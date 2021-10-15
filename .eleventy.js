5a0381052348f7a1660be7c4be402e890dd0cd9e
emilasbringer <emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.se>
2021-10-15 11:12:51 +0200

    ✨ 🎨

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css/');
     eleventyConfig.addWatchTarget('./src/css/');
+    eleventyConfig.addPassthroughCopy('./src/img')
+    eleventyConfig.addPassthroughCopy('./src/scripts')
 
     eleventyConfig.addPlugin(syntaxHighlight);
     return {
